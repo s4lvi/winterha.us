41c702e09a5f1a9cdb59f85e1ecb5cebdd131fd7
s4lvi <salvicomputers@gmail.com>
2022-02-19 14:55:41 -0500

    tag style

--- portfolio22/src/ProjectInfo/ProjectInfo.js
+++ portfolio22/src/ProjectInfo/ProjectInfo.js
@@ -5,7 +5,7 @@ class ProjectInfo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            project: props.project
+            project: null
         }
     }
     
@@ -14,12 +14,20 @@ class ProjectInfo extends React.Component {
     }
 
     render() {
+        var tags = [];
+        if (this.state.project && this.state.project.tags && this.state.project.tags.length > 0) {
+            tags = this.state.project.tags.map(t => {
+                return <li key={t}>{t}</li>
+            })
+        }
         return (
             <React.Fragment>
-                { this.state.project &&
+                { this.state.project && this.state.project.tags && 
                     <div className="project-info">
                         <a href={this.state.project.link}>{this.state.project.title}</a><br />
-                        {this.state.project.description}<br />
+                        {this.state.project.description}<br /><br />
+                        {"tags: "}<br /><br />
+                        <ul className="tag-list">{tags}</ul>
                     </div>
                 }
             </React.Fragment>
