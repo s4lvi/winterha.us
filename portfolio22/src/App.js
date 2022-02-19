bd1940dfdd708628c247e3af736f091cd774b510
s4lvi <salvicomputers@gmail.com>
2022-02-18 22:46:03 -0500

    Working on layout

--- portfolio22/src/App.js
+++ portfolio22/src/App.js
@@ -1,24 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import ProjectList from './ProjectList/ProjectList';
-import ProjectInfo from './ProjectInfo/ProjectInfo';
+import Portfolio from './Porfolio/Portfolio';
 
 function App() {
   return (
-    <div className="page">
-      <div className="inner-page">
-        <div className="title">salvi / codeweekend portfolio</div>
-        <hr />
-        <div className="content">
-          <div className="content-left">
-            <ProjectList />
-          </div>
-          <div className="content-right">
-            <ProjectInfo />
-          </div>
-        </div>
-      </div>
-    </div>
+      <Portfolio />
   );
 }
 
