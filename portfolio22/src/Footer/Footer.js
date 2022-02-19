c5b6ee8156b57c27b27521e03ca2656d6d4d6189
s4lvi <salvicomputers@gmail.com>
2022-02-19 15:16:17 -0500

    Added social icons

--- portfolio22/src/Footer/Footer.js
+++ portfolio22/src/Footer/Footer.js
@@ -1,4 +1,6 @@
-import * as React from "react"
+import * as React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 class Footer extends React.Component {
 
@@ -6,6 +8,10 @@ class Footer extends React.Component {
         return (
             <div className="footer">
                 <p>&copy; 2022 jordan salvi</p>
+                <div>
+                    <a href="https://github.com/s4lvi"><GitHubIcon sx={{ color: '#160C28', marginRight:"1em"}} /></a>
+                    <a href="https://twitter.com/mivlas"><TwitterIcon sx={{ color: '#160C28' }} /></a>
+                </div>
             </div>
         )
     }
