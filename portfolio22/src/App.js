6cb3a1ee02f1996f23eda8f634eebea3a81ae026
s4lvi <salvicomputers@gmail.com>
2022-02-18 22:43:58 -0500

    Working on layout

--- portfolio22/src/App.js
+++ portfolio22/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import ProjectList from './ProjectList/ProjectList';
+import ProjectInfo from './ProjectInfo/ProjectInfo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <div className="inner-page">
+        <div className="title">salvi / codeweekend portfolio</div>
+        <hr />
+        <div className="content">
+          <div className="content-left">
+            <ProjectList />
+          </div>
+          <div className="content-right">
+            <ProjectInfo />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
