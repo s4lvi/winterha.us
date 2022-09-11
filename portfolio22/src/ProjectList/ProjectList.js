377ec1a355b50b3fed939b0b90a540c872ce7739
s4lvi <salvicomputers@gmail.com>
2022-09-11 01:34:58 -0400

    added tag highlight

--- portfolio22/src/ProjectList/ProjectList.js
+++ portfolio22/src/ProjectList/ProjectList.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from "react"
 
 const ProjectList = (props) => {
     const [projects, setProjects] = useState([])
-    const [tag, setTag] = useState()
+    //const [tag, setTag] = useState()
     const [tagList, setTagList] = useState([])
 
     const select = (title) => {
@@ -10,15 +10,37 @@ const ProjectList = (props) => {
     }
     
     const selectTag = (tag) => {
-        setTag(tag)
+        updateProjects(tag)
+    }
+
+    const updateProjects = (tag=null) => {
+        if (props.projects && Object.keys(props.projects).length > 0) {
+            setProjects(Object.keys(props.projects).map(p => {
+                var highlight = props.projects[p].tags.includes(tag) ? 'tag-highlight' : ''
+                return <li 
+                        onMouseEnter={() => select(props.projects[p].title)} 
+                        key={props.projects[p].title}
+                        className={highlight}> 
+                            {"> "}{props.projects[p].title}
+                    </li>
+            }))
+        }
     }
 
     useEffect(() => {
+        updateProjects()
         if (props.projects && Object.keys(props.projects).length > 0) {
-            setProjects(Object.keys(props.projects).map(p => {
-                return <li onMouseEnter={() => select(props.projects[p].title)} key={props.projects[p].title}>{"> "}{props.projects[p].title}</li>
+            var tags = new Set()
+            Object.keys(props.projects).map(p => {
+                props.projects[p].tags.map(t => {
+                        tags.add(t)
+                })
+            })
+            setTagList(Array.from(tags).map(t => {
+                return <li onClick={() => selectTag(t)} key={t}>{t}</li>
             }))
         }
+
     }, [props.projects])
 
     return (
@@ -26,8 +48,10 @@ const ProjectList = (props) => {
             {projects.length > 0 &&
                 <ul className="title-list">{projects}</ul>
             }
+            <hr />
+            tags:
             {tagList.length > 0 && 
-                <ul className="title-list">{tagList}</ul>
+                <ul className="tag-list">{tagList}</ul>
             }
         </React.Fragment>
     )
